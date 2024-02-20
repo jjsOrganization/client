283cdfa06d4956c74baf35082b56e3779f34bfb3
YJK <yjg0557@naver.com>
2024-02-20 17:13:31 +0900

    상품 등록 수정 삭제와 로그인 연동

--- src/App.js
+++ src/App.js
@@ -17,7 +17,6 @@ import CustomerMypageOrderList from "./js/CustomerMypageOrderList.js";
 import CustomerShoppingBasket from "./js/CustomerShoppingBasket.js";
 import { Routes, Route, useNavigate } from "react-router-dom";
 import exProductURL from "./images/exProduct.jpg";
-import axios from 'axios';
 
 import { Button, Nav } from "react-bootstrap";
 import DesignerSearch from "./pages/DesignerSearch";
@@ -37,6 +36,7 @@ const App = () => {
   
 
   let [onOff, setOnOff] = useState(false);
+  let [shoes, setShoes] = useState(data);
   let navigate = useNavigate();
 
   //서버에서 받은 데이터 이용해 메인 상품 추가
@@ -51,77 +51,6 @@ const App = () => {
   const [selectedTab, setSelectedTab] = useState(1);
   const [userEmail, setUserEmail] = useState("");
   const [password, setPassword] = useState("");
-<<<<<<< HEAD
-=======
-  const itemsPerPage = 6; // 페이지당 보여줄 상품 수
-  const [productData,setProductData] = useState();
-  const products = [
-    {
-      id: 1,
-      name: "상품 1",
-      images: [exProductURL],
-    },
-    {
-      id: 2,
-      name: "상품 2",
-      images: [exProductURL],
-    },
-    {
-      id: 3,
-      name: "상품 3",
-      images: [exProductURL],
-    },
-    {
-      id: 4,
-      name: "상품 4",
-      images: [exProductURL],
-    },
-    {
-      id: 5,
-      name: "상품 5",
-      images: [exProductURL],
-    },
-    {
-      id: 6,
-      name: "상품 6",
-      images: [exProductURL],
-    },
-    {
-      id: 7,
-      name: "상품 7",
-      images: [exProductURL],
-    },
-    {
-      id: 8,
-      name: "상품 8",
-      images: [exProductURL],
-    },
-    {
-      id: 9,
-      name: "상품 9",
-      images: [exProductURL],
-    },
-  ];
->>>>>>> 518cfcc42f70204099e14a6a6db3f9f10e6afa08
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(`/product/all`, {
-          headers: {
-            'Content-Type': 'multipart/form-data',
-            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
-            'X-CSRF-TOKEN': localStorage.getItem('csrfToken'),
-          }
-        });
-        setProductData(response.data); // 수정된 부분
-      } catch(error) {
-        console.log('데이터 로드 실패', error);
-      }
-    }
-    fetchData();
-  }, [])
-  
 
   return (
     <div>
@@ -179,15 +108,18 @@ const App = () => {
             }
           ></Route>
 
-          <Route path="/detail/:productid" element={<Detail productDetailInfo = {productData} />} /><Route />
+          <Route path="/detail/:id" element={<Detail shoes={shoes} />} />
+          <Route />
 
           <Route path="/productupdate" element={<ProductUpdate />}></Route>
 
           <Route
             path="/stockList"
-            element={<StockList data={data} mainImage={mainImage} />}></Route>
+            element={<StockList data={data} mainImage={mainImage} />}
+          ></Route>
 
-          <Route path="/stockupdater/:id" element={<StockUpdater />} /><Route />
+          <Route path="/stockupdater/:id" element={<StockUpdater />} />
+          <Route />
 
           <Route path="/mypage2" element={<MyPages data={data} />}></Route>
         </Routes>
