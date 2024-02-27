8f5ef3e653323ad19762ed612c5ffcbf9825a17a
YJK <yjg0557@naver.com>
2024-02-27 17:29:56 +0900

    상품상세에서 장바구니 추가 및 리폼요청

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,14 +1,13 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import styled from "styled-components";
 import { useEffect, useState } from "react";
 import Dropdown from "../component/dropdown";
 import copyType from "../component/dropdown";
 import { Routes, Route, useNavigate } from "react-router-dom";
-import axios from 'axios';
+import axios from "axios";
 import "../js/TopBar.js";
 import TopBar from "../js/TopBar.js";
-
 
 let BasicBtn = styled.button`
   padding: 1%;
@@ -23,8 +22,8 @@ function Detail(props) {
   let [total, setTotal] = useState(0);
   const [dropdownVisibility, setDropdownVisibility] = React.useState(false);
   let { productid } = useParams();
-  const [productidInt,setProductidInt] = useState();
-  const [productDetailInfo,setProductDetailInfo] = useState();
+  const [productidInt, setProductidInt] = useState();
+  const [productDetailInfo, setProductDetailInfo] = useState();
   const [salePrice, setSalePrice] = useState();
 
   //Dropdown 관련 변수
@@ -53,38 +52,68 @@ function Detail(props) {
   }, [choiceSize, choiceColor]);
 
   useEffect(() => {
-    setProductidInt(parseInt(productid))
+    setProductidInt(parseInt(productid));
     const fetchData = async () => {
-    try {
+      try {
         const response = await axios.get(`/product/all/detail/${productid}`, {
-        headers: {
-            'Content-Type': 'multipart/form-data',
-            'Authorization': `Bearer ${localStorage.getItem('accessToken')}`,
-            'X-CSRF-TOKEN': localStorage.getItem('csrfToken'),
-        }
+          headers: {
+            "Content-Type": "multipart/form-data",
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            "X-CSRF-TOKEN": localStorage.getItem("csrfToken"),
+          },
         });
         setProductDetailInfo(response.data);
-    } catch(error) {
-        console.log('상품 상세 데이터 로드 실패', error);
-    }
-    }
+      } catch (error) {
+        console.log("상품 상세 데이터 로드 실패", error);
+      }
+    };
     fetchData();
-}, [])
+  }, []);
 
-useEffect(() => {
-  if (productDetailInfo) {
+  useEffect(() => {
+    if (productDetailInfo) {
+      console.log(productDetailInfo);
       console.log(productDetailInfo.productName);
       setSalePrice(productDetailInfo.price * (1 - sale));
-  } else {
-      console.log('상품 데이터가 아직 로드되지 않았습니다.');
+    } else {
+      console.log("상품 데이터가 아직 로드되지 않았습니다.");
+    }
+  }, [productDetailInfo, sale]);
+
+  if (!productDetailInfo) {
+    return <div>데이터를 로드하는 중입니다...</div>;
   }
-}, [productDetailInfo, sale]);
 
-if (!productDetailInfo) {
-  return <div>데이터를 로드하는 중입니다...</div>;
-}
+  const addToCart = async () => {
+    try {
+      const response = await axios.post(
+        `/cart/purchaser/add/${productid}`,
+        {
+          id: productDetailInfo.id,
+          itemDetail: productDetailInfo.itemDetail,
+          price: productDetailInfo.price,
+          productImg: productDetailInfo.productImg,
+          productName: productDetailInfo.productName,
+          productSellStatus: productDetailInfo.productSellStatus,
+        },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+            "X-CSRF-TOKEN": localStorage.getItem("csrfToken"),
+          },
+        }
+      );
+      console.log("장바구니에 상품 추가:", response.data);
+      // 크롬 알림표시 사용 예정
+    } catch (error) {
+      console.error("장바구니에 상품 추가 실패:", error);
+    }
+  };
 
-return (
+  const reFormLink = `/reform?productId=${productid}`;
+
+  return (
     <div className="Detail">
       <TopBar />
       <div
@@ -122,7 +151,9 @@ return (
             {productDetailInfo.price}
           </p>
           <p style={{ fontWeight: "800", color: "red" }}>{sale * 100}%</p>
-          <p style={{ fontWeight: "bold" }}>할인가 : {salePrice && salePrice.toLocaleString()} </p>
+          <p style={{ fontWeight: "bold" }}>
+            할인가 : {salePrice && salePrice.toLocaleString()}{" "}
+          </p>
           <div
             className="btn"
             style={{
@@ -131,7 +162,7 @@ return (
               position: "absolute",
               bottom: "0",
               w0th: "100%",
-              width : '100%'
+              width: "100%",
             }}
           >
             <BasicBtn
@@ -140,9 +171,14 @@ return (
                   ? alert("사이즈와 색상을 선택해 주세요")
                   : alert("구매 완료");
                 window.location.replace(`/detail/${productid}`);
-              }}>구매하기</BasicBtn>
-            <BasicBtn>의뢰하기</BasicBtn>
-            <BasicBtn>장바구니</BasicBtn>
+              }}
+            >
+              구매하기
+            </BasicBtn>
+            <BasicBtn>
+              <Link to={reFormLink}>의뢰하기</Link>
+            </BasicBtn>
+            <BasicBtn onClick={addToCart}>장바구니</BasicBtn>
           </div>
         </div>
       </div>
@@ -191,8 +227,4 @@ return (
   );
 }
 
-
-
 export default Detail;
-
-
