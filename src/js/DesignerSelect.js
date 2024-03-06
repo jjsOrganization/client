cefaa6bb5e9ef32c89225f57e19dd18b9381fe3a
YJK <yjg0557@naver.com>
2024-03-06 22:09:20 +0900

    회원가입 구현

--- src/js/DesignerSelect.js
+++ src/js/DesignerSelect.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 import React from "react";
 
 function DesignerSelect() {
+  // window.opener.handleSelectedDesigner(selectedDesigner);
   const [designersList, setDesignersList] = useState(null);
 
   useEffect(() => {
