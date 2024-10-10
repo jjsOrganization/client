1b9d9eb06e4cd25a5f7f744ac3a649f064f5af73
YJKBigBo <yjg0557@naver.com>
2024-10-10 16:55:29 +0900

    refactor : 디자이너 마이페이지 포트폴리오 수정 (API 추가 예정)

--- src/pages/ModifyPortfolio.js
+++ src/pages/ModifyPortfolio.js
@@ -1,186 +1,236 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
-import { useNavigate, useParams } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
 import axiosInstance from "../component/jwt.js";
+import TopBar from "../component/TopBar.js";
+import { PhotoIcon } from '@heroicons/react/24/solid';
+import { useNavigate } from "react-router-dom";
 
-const Container = styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-`;
+const ModifyPortfolio = (props) => {
+  const [thumbnailImage, setThumbnailImage] = useState();
+  const [thumbnailImageFile, setThumbnailImageFile] = useState();
+  const [designerName, setDesignerName] = useState('');
+  const [designerIntro, setDesignerIntro] = useState('');
+  const [priceImg, setPriceImg] = useState();
+  const [priceThumbnailImageFile, setPriceThumbnailImageFile] = useState();
+  const [designerProfile, setDesignerProfile] = useState({});
+  const [designerEmail,setDesignerEmail] = useState('');
+  let navigate = useNavigate();
 
-const ProfileSection = styled.div`
-display: flex;
-align-items: flex-start;
-margin-bottom: 20px;
-`;
+  const saveName = (event) => {
+    setDesignerName(event.target.value);
+  };
 
-const ProfileImage = styled.img`
-width: 300px;
-height: 225px;
-margin-right: 20px;
-`;
+  const saveIntro = (event) => {
+    setDesignerIntro(event.target.value);
+  };
 
-const DesignerIntro = styled.div`
-max-width: 600px;
-`;
+  const encodeImageFile = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
 
-const Textarea = styled.textarea`
-width: 100%;
-height: 100px;
-margin-bottom: 10px;
-`;
+    reader.onload = () => {
+      setThumbnailImage(reader.result);
+      setThumbnailImageFile(file);
+    };
+  };
 
-const Button = styled.button`
-background-color: black;
-color: white;
-padding: 10px 20px;
-border: none;
-border-radius: 5px;
-cursor: pointer;
-margin-right: 10px;
-`;
+  const encodePriceImageFile = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
 
-const Table = styled.table`
-border-collapse: collapse;
-width: 100%;
-`;
+    reader.onload = () => {
+      setPriceImg(reader.result);
+      setPriceThumbnailImageFile(file);
+    };
+  };
 
-const Th = styled.th`
-border: 1px solid black;
-padding: 8px;
-`;
-
-const Td = styled.td`
-border: 1px solid black;
-padding: 8px;
-`;
-
-const Input = styled.input`
-width: 80px;
-`;
-
-const FileInput = styled.input`
-margin-top: 10px;
-`;
-
-const ModifyPortfolio = () => {
-const portfolidId = useParams()
-const navigate = useNavigate();
-const [designerIntro, setDesignerIntro] = useState("소개글을 작성해주세요.");
-const [profileImage, setProfileImage] = useState("");
-const [portfolio, setPortfolio] = useState({
-    explanation: "",
-    designerImage: "",
-    designerName: "",
-});
-
-const [isEditingIntro, setIsEditingIntro] = useState(false);
-
-const handleFinishEditingIntro = () => {
-setIsEditingIntro(false);
-};
-
-const handleImageUpload = (event) => {
-const imageFile = event.target.files[0];
-const imageUrl = URL.createObjectURL(imageFile);
-setProfileImage(imageUrl);
-};
-
-const gotoRegisterPage = () => {
-navigate("/DesignerMypage/ModifyPortfolio/RegisterPortfolio");
-};
-
-const handleRegister = async () => {
+  const registerHandler = async () => {
     try {
-    const formData = new FormData();
-    formData.append("explanation", portfolio.explanation);
-    formData.append("designerImage", portfolio.designerImage);
-    formData.append("designerName", portfolio.designerName);
-    const response = await axiosInstance.put(
-        `/portfolio/designer/${portfolidId}`,
+      const formData = new FormData();
+      formData.append("explanation", designerIntro);
+      formData.append("designerImage", thumbnailImageFile);
+      formData.append("designerName", designerName);
+      formData.append("designerEmail", designerEmail);
+      formData.append("priceImage", priceThumbnailImageFile);
+      const response = await axiosInstance.put(
+        "/portfolio/designer/5",
         formData,
         {
-        headers: {
-            "Content-Type": "multipart/form-data",
+          headers: {
             Authorization: `Bearer ${localStorage.getItem("accessToken")}`,
-        },
+          },
         }
-    );
+      );
+      window.location.replace(`/DesignerMypage`);
+      alert("디자이너 포트폴리오가 수정되었습니다.");
     } catch (error) {
+      alert("디자이너 포트폴리오 수정에 실패하였습니다. 다시 확인해주세요.");
     }
-};
+  };
 
+  const cancelHandler = () => {
+    navigate("/DesignerMyPage");
+  };
 
+  const fetchDesignerMypage = async () => {
+    try {
+      const responseDesignerInfo = await axiosInstance.get("/portfolio/designer");
+      const infoData = responseDesignerInfo.data;
+  
+      setDesignerName(infoData.designerName);
+      setDesignerIntro(infoData.explanation);
+      setDesignerEmail(infoData.designerEmail);
+  
+      if (infoData.designerImagePath) {
+        const thumbnailFile = await urlToFile(infoData.designerImagePath, "designerImage.jpg");
+        setThumbnailImage(infoData.designerImagePath);
+        setThumbnailImageFile(thumbnailFile);
+      }
 
-return (
-<Container>
-    <h2>디자이너 포트폴리오 수정</h2>
-    
-    <ProfileSection>
-    <ProfileImage src={profileImage} alt="프로필 이미지" />
-    <DesignerIntro>
-        <h3>디자이너 소개</h3>
-        {isEditingIntro ? (
-        <>
-            <Textarea 
-            value={designerIntro}
-            onChange={(e) => setDesignerIntro(e.target.value)}
-            />
-            <Button onClick={handleFinishEditingIntro}>수정 완료</Button>
-        </>
-        ) : (
-        <>
-            <p>{designerIntro}</p>
-            <Button onClick={() => setIsEditingIntro(true)}>수정</Button>
-        </>
-        )}
-    </DesignerIntro>
-    </ProfileSection>
-    
-    {/* 프로필 사진 업로드 입력란 */}
-    <FileInput type="file" accept="image/*" onChange={handleImageUpload} />
-    
-    {/* 리폼 가격표 */}
-    <h3>리폼 가격표</h3>
-    <Table>
-    <thead>
-        <tr>
-        <Th>리폼부분</Th>
-        <Th>가격</Th>
-        <Th>리폼부분</Th>
-        <Th>가격</Th>
-        </tr>
-    </thead>
-    <tbody>
-        <tr>
-        <Td>디자인</Td>
-        <Td>
-            <Input type="text" placeholder="가격 입력" />
-        </Td>
-        <Td>기장</Td>
-        <Td>
-            <Input type="text" placeholder="가격 입력" />
-        </Td>
-        </tr>
-        <tr>
-        <Td>사이즈</Td>
-        <Td>
-            <Input type="text" placeholder="가격 입력" />
-        </Td>
-        <Td>장식</Td>
-        <Td>
-            <Input type="text" placeholder="가격 입력" />
-        </Td>
-        </tr>
-    </tbody>
-    </Table>
-    
-    <h2>작업물 리스트</h2>
-    
-    <Button onClick={handleRegister}>작업물 등록</Button>
-</Container>
-);
+      if (infoData.priceImagePath) {
+        const priceFile = await urlToFile(infoData.priceImagePath, "priceImage.jpg");
+        setPriceImg(infoData.priceImagePath);
+        setPriceThumbnailImageFile(priceFile);
+      }
+    } catch (error) {
+      alert("예상치 못한 오류 다시 시도해주세요.");
+    }
+  };
+  
+  const urlToFile = async (url, filename) => {
+    const response = await fetch(url);
+    const blob = await response.blob();
+    return new File([blob], filename, { type: blob.type });
+  };
+
+  useEffect(() => {
+    fetchDesignerMypage();
+  }, []);
+
+  return (
+    <div>
+      <TopBar />
+      <form className="formWrapper mx-auto max-w-4xl px-4 my-5">
+        <div className="space-y-12">
+          <div className="border-b border-gray-900/10 pb-12">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">포트폴리오 수정</h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              등록한 디자이너 포트폴리오 정보가 구매자들에게 나타납니다. 
+            </p>
+
+            <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+              
+              <div className="col-span-full">
+                <label htmlFor="designer-image-upload" className="block text-sm font-medium leading-6 text-gray-900">
+                  디자이너 사진
+                </label>
+                <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                  <div className="text-center">
+                    {thumbnailImage && (
+                      <img src={thumbnailImage} alt="Thumbnail" className="mx-auto h-40 w-auto" />
+                    )}
+                    
+                    <div className="mt-4 flex text-sm leading-6 text-gray-600 text-center">
+                      <label
+                        htmlFor="designer-image-upload"
+                        className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                      >
+                        <span>Upload a file</span>
+                        <input id="designer-image-upload" name="file-upload" type="file" className="sr-only" onChange={encodeImageFile} />
+                      </label>
+                      <p className="pl-1">or drag and drop</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-span-full">
+                <label htmlFor="price-image-upload" className="block text-sm font-medium leading-6 text-gray-900">
+                  가격 사진
+                </label>
+                <div className="mt-2 flex justify-center rounded-lg border border-dashed border-gray-900/25 px-6 py-10">
+                  <div className="text-center">
+                    {priceImg && (
+                      <img src={priceImg} alt="Price" className="mx-auto h-40 w-auto" />
+                    )}
+                    {!priceImg && (
+                      <PhotoIcon className="mx-auto h-15 w-15 text-gray-300" aria-hidden="true" />
+                    )}
+                    <div className="mt-4 flex text-sm leading-6 text-gray-600 text-center">
+                      <label
+                        htmlFor="price-image-upload"
+                        className="relative cursor-pointer rounded-md bg-white font-semibold text-indigo-600 focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 hover:text-indigo-500"
+                      >
+                        <span>Upload a file</span>
+                        <input id="price-image-upload" name="file-upload" type="file" className="sr-only" onChange={encodePriceImageFile} />
+                      </label>
+                      <p className="pl-1">or drag and drop</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="sm:col-span-4">
+                <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
+                  디자이너 명
+                </label>
+                <div className="mt-2">
+                  <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                    <input
+                      type="text"
+                      name="username"
+                      id="username"
+                      autoComplete="username"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                      placeholder="janesmith"
+                      value={designerName}
+                      onChange={saveName}
+                    />
+                  </div>
+                </div>
+              </div>
+
+              <div className="col-span-full">
+                <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900">
+                  소개
+                </label>
+                <div className="mt-2">
+                  <textarea
+                    id="about"
+                    name="about"
+                    rows={3}
+                    className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={designerIntro}
+                    onChange={saveIntro}
+                  />
+                </div>
+                <p className="mt-3 text-sm leading-6 text-gray-600">당신을 소개하는 글을 간단하게 작성해주세요.</p>
+              </div>
+              
+            </div>
+          </div>
+        </div>
+
+        <div className="mt-6 flex items-center justify-end gap-x-6">
+          <div 
+            type="button" 
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={cancelHandler}
+          >
+            취소
+          </div>
+          <div
+            type="button"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={registerHandler}
+          >
+            수정
+          </div>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default ModifyPortfolio;
